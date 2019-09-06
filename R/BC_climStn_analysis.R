b1f06b661827ed227b081ecf9e8d0a7d5cfd6f6f
Griesbauer <Hardy.Griesbauer@gov.bc.ca>
2019-09-06 15:38:01 -0700

    sd

--- R/BC_climStn_analysis.R
+++ R/BC_climStn_analysis.R
@@ -1,4 +1,5 @@
 # Script to concatenate BGC units with climate stations in BC
+# Then select for a subset of BGC units that you are interested in.
 
 load(here::here("data","BC_clim_stn_metadata.RData"))
 
@@ -28,4 +29,14 @@ stn<-st_join(stn,bgc[,"MAP_LABEL"])
 #   group_by(MAP_LABEL) %>%
 #   summarise(Num.Stn=n()) %>%
 #   write.csv(file="Climate stations by BGC unit.csv")
+
+# Shortlist stn list for BGC units of interest
+bgcList<-c("BWBSdk","SBSmk1","SBSmk2","SBSwk2","ESSFmv2","ESSFmvp")
+stnList<-stn[which(stn$MAP_LABEL%in%bgcList==TRUE),]
+
+# Shortlist only for EnvCan, BC Hydro, or Env-ASP stations
+stnList %>%
+  dplyr::filter(network_name=="EC" | network_name=="EC_raw" | network_name=="BCH") %>%
+  write.csv(file="Mackenzie_drought_Climate stations by BGC unit.csv")
+
 
